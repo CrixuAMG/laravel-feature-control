3cf600f371fd10c6b4b6f23705742e555e2025b3
Christian Kaal <christiankaal@gmail.com>
2022-07-14 21:34:35 +0200

    WIP

--- src/Services/AbstractFeature.php
+++ src/Services/AbstractFeature.php
@@ -2,15 +2,17 @@
 
 namespace CrixuAMG\FeatureControl\Services;
 
+use CrixuAMG\FeatureControl\Models\Feature;
+
 abstract class AbstractFeature
 {
     abstract public function getKey(): string;
 
     public function shouldRelease(): bool
     {
-        return FeatureControl::where('key', $this->getKey())
-            ->where('roll_out_per_user', false)
-            ->first()
-            ?->enabled === false;
+        return Feature::where('key', $this->getKey())
+                ->where('roll_out_per_user', false)
+                ->first()
+                ?->enabled === false;
     }
 }
