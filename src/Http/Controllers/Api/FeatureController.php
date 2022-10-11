190e2e7807cf5e654fb78a3f835132addbd97a74
Christian Kaal <christiankaal@gmail.com>
2022-10-11 11:26:01 +0200

    Use feature resource

--- src/Http/Controllers/Api/FeatureController.php
+++ src/Http/Controllers/Api/FeatureController.php
@@ -2,12 +2,15 @@
 
 namespace CrixuAMG\FeatureControl\Http\Controllers\Api;
 
+use CrixuAMG\FeatureControl\Http\Resource\FeatureResource;
 use CrixuAMG\FeatureControl\Models\Feature;
 
 class FeatureController
 {
     public function index()
     {
-        return request()->per_page > 0 ? Feature::paginate() : Feature::all();
+        return FeatureResource::collection(
+            request()->per_page > 0 ? Feature::paginate() : Feature::all()
+        );
     }
 }
