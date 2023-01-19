50baca0df185f5e1d93d6aee2cc97b38b21d1ee3
Christian Kaal <christiankaal@gmail.com>
2023-01-19 10:59:31 +0100

    Added support for user features

--- src/Http/Controllers/Api/FeatureController.php
+++ src/Http/Controllers/Api/FeatureController.php
@@ -2,6 +2,7 @@
 
 namespace CrixuAMG\FeatureControl\Http\Controllers\Api;
 
+use Illuminate\Support\Collection;
 use CrixuAMG\FeatureControl\Http\Resource\FeatureResource;
 use CrixuAMG\FeatureControl\Models\Feature;
 
@@ -11,9 +12,32 @@ class FeatureController
     {
         $method = request()->per_page > 0 ? 'paginate' : 'get';
 
-        return FeatureResource::collection(
-            Feature::where('retired', false)
-                ->$method()
-        );
+        /** @var Collection $features */
+        $features = Feature::where('retired', false)
+            ->$method();
+
+        if ($user = request()->user()) {
+            $userEnabledFeatures = $user->features()
+                ->whereNotIn('id', $features->where('enabled', true)->pluck('id')->all())
+                ->where('retired', false)
+                ->get();
+
+            foreach ($userEnabledFeatures as $feature) {
+                if ($features->contains($feature)) {
+                    $features = $features->map(function ($featureItem) use ($feature) {
+                        if ($featureItem->id === $feature->id) {
+                            $feature->enabled = true;
+                            return $feature;
+                        }
+
+                        return $featureItem;
+                    });
+                } else {
+                    $features->push($feature);
+                }
+            }
+        }
+
+        return FeatureResource::collection($features);
     }
 }
